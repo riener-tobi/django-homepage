e75ab45118f2ee54dd084af074248ac39bba0ebc
Tobias Riener <to.riener@gmail.com>
2024-11-06 17:13:27 +0100

    added allowed hosts

--- webserver/settings.py
+++ webserver/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = 'django-insecure-b347u+5ws1la0)llk7@m6lh+tf(t)au%z-xoa7zzguclonvi31
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhos',
+    '127.0.0.1',
+    '10.0.0.190',
+]
 
 
 # Application definition
